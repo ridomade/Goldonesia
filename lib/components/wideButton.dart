123c6acb69661b7053d48bf8f749fad8264da387
Gerald Shabran <electrobyt3@gmail.com>
2024-01-09 16:32:10 +0700

    mayor changes in login and make a sign up screen

--- lib/components/wideButton.dart
+++ lib/components/wideButton.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
-class loginButton extends StatelessWidget {
+class wideButton extends StatelessWidget {
   final Function()? onTap;
+  final String text;
 
-  const loginButton({super.key, this.onTap});
+  const wideButton({super.key, this.onTap, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.all(25),
-        margin: EdgeInsets.symmetric(horizontal: 25),
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
           color: Colors.black,
           borderRadius: BorderRadius.circular(8),
         ),
-        child: const Center(
+        child: Center(
           child: Text(
-            "Login",
-            style: TextStyle(
+            text,
+            style: const TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 16,
--- lib/screen/signUp.dart
+++ lib/screen/signUp.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:goldonesia/components/wideButton.dart';
 import 'package:goldonesia/components/reusableTextfield.dart';
-import 'package:goldonesia/screen/signUp.dart';
+import 'package:goldonesia/components/wideButton.dart';
+import 'package:goldonesia/screen/loginPage.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class SignUp extends StatelessWidget {
+  SignUp({super.key});
 
   //controller
+  final usernameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
 
   // log user in
-  void logInUser() {} // enter firebase auth
+  void signUpUser() {} // enter firebase auth
 
   @override
   Widget build(BuildContext context) {
@@ -24,23 +26,31 @@ class LoginPage extends StatelessWidget {
               children: [
                 const SizedBox(height: 50),
 
-                // logo
+                //logo
                 const Icon(
-                  Icons.lock_outline_rounded,
+                  Icons.person,
                   size: 100,
-                ),
-
-                const SizedBox(height: 50),
-
-                // hello there!
-                Text(
-                  'Hello There!',
-                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
                 ),
 
                 const SizedBox(height: 25),
 
-                // Email
+                Text(
+                  'Let\'s make an account for you',
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                ),
+
+                const SizedBox(height: 50),
+
+                //Username
+                ReusableTextfield(
+                  controller: usernameController,
+                  hintText: "Username",
+                  obsecureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                //email
                 ReusableTextfield(
                   controller: emailController,
                   hintText: "Email",
@@ -49,42 +59,32 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 10),
 
-                // Password
+                //Password
                 ReusableTextfield(
                   controller: passwordController,
-                  hintText: "password",
+                  hintText: "Password",
                   obsecureText: true,
                 ),
 
                 const SizedBox(height: 10),
 
-                // Forgot Password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Forgot Password?",
-                        style: TextStyle(
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                    ],
-                  ),
+                //confirm password
+                ReusableTextfield(
+                  controller: confirmPasswordController,
+                  hintText: "Confirm Password",
+                  obsecureText: true,
                 ),
 
-                const SizedBox(height: 20),
+                const SizedBox(height: 25),
 
-                // login button
                 wideButton(
-                  text: "Login",
-                  onTap: logInUser,
+                  text: "Sign Up",
+                  onTap: signUpUser,
                 ),
 
-                const SizedBox(height: 50),
+                const SizedBox(height: 25),
 
-                // or continue with
+                //have an acoount? log in right away!
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
                   child: Row(
@@ -99,18 +99,18 @@ class LoginPage extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(horizontal: 10),
                         child: Row(
                           children: [
-                            const Text("dont have an account yet? "),
+                            const Text("Have an account? "),
                             GestureDetector(
                               onTap: () {
                                 // Navigate to the registration screen
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => SignUp()),
+                                      builder: (context) => LoginPage()),
                                 );
                               },
                               child: const Text(
-                                "register now!",
+                                "Login right away!",
                                 style: TextStyle(
                                     color: Colors.blue,
                                     fontWeight: FontWeight.bold),
