69c3017c6c32be85d1c7f95006987ace24410a27
Gerald Shabran <electrobyt3@gmail.com>
2024-01-16 13:56:32 +0700

    restyled edit page and listIdea

--- lib/screen/edit.dart
+++ lib/screen/edit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:goldonesia/components/reusableTextfield.dart';
+import 'package:goldonesia/constants/color.dart';
 
 import '../models/note.dart';
 
@@ -31,96 +33,111 @@ class _EditScreenState extends State<EditScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade900,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  padding: const EdgeInsets.all(0),
-                  icon: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade800.withOpacity(.8),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: ListView(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            lightGreen,
+            lightGreen,
+            white,
+            white,
+            white,
+            white,
+            white,
+            teal,
+            teal
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextField(
-                    controller: _titleController,
-                    style: const TextStyle(color: Colors.white, fontSize: 30),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Title',
-                      hintStyle: TextStyle(color: Colors.grey, fontSize: 30),
-                    ),
-                  ),
-                  TextField(
-                    controller: _contentController,
-                    style: const TextStyle(color: Colors.white),
-                    maxLines: null,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Type something here',
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  TextField(
-                    controller: _danaAwalController,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Dana Awal',
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  TextField(
-                    controller: _untungController,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Untung',
-                      hintStyle: TextStyle(color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    padding: const EdgeInsets.all(0),
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade800.withOpacity(.8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(
-            context,
-            [
-              _titleController.text,
-              _contentController.text,
-              _danaAwalController.text,
-              _untungController.text,
+              Expanded(
+                child: ListView(
+                  children: [
+                    Center(
+                      child: Text(
+                        "Lengkapi Idemu",
+                        style: TextStyle(
+                          fontFamily: 'Odudo-Soft',
+                          color: Color(0xFF0766AD),
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign:
+                            TextAlign.center, // Center the text within the row
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ReusableTextField(
+                        label: "Judul Ide",
+                        controller: _titleController,
+                        obscureText: false),
+                    SizedBox(height: 10),
+                    ReusableTextField(
+                        label: "Deskripsi Ide",
+                        controller: _contentController,
+                        obscureText: false),
+                    SizedBox(height: 10),
+                    ReusableTextField(
+                        label: "Dana Awal",
+                        controller: _danaAwalController,
+                        obscureText: false),
+                    SizedBox(height: 10),
+                    ReusableTextField(
+                        label: "Perkiraan Untung",
+                        controller: _untungController,
+                        obscureText: false),
+                  ],
+                ),
+              ),
             ],
-          );
-        },
-        elevation: 10,
-        backgroundColor: Colors.grey.shade800,
-        child: const Icon(Icons.save),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(
+              context,
+              [
+                _titleController.text,
+                _contentController.text,
+                _danaAwalController.text,
+                _untungController.text,
+              ],
+            );
+          },
+          elevation: 10,
+          backgroundColor: lightGreen,
+          child: const Icon(Icons.save, color: Colors.teal),
+        ),
       ),
     );
   }
